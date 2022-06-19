edcc8e8f05d84163cf83e8916ce1fde72498384b
suekichi27 <102605397+suekichi27@users.noreply.github.com>
2022-06-19 19:26:57 +0300

    Update main.js

--- app/main.js
+++ app/main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const start = async() => {
         const mindarThree = new window.MINDAR.IMAGE.MindARThree({
             container: document.body,
-            imageTargetSrc: '../assets/targets/target.mind'
+            imageTargetSrc: './assets/targets/target.mind'
         });
         
         const {renderer, scene, camera} = mindarThree;
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
         scene.add(light);
 
-        const fish_mart = await loadGLTF('../assets/models/fish_mart/scene.gltf');
+        const fish_mart = await loadGLTF('./assets/models/fish_mart/scene.gltf');
         fish_mart.scene.scale.set(0.002, 0.002, 0.002);
         fish_mart.scene.rotation.set(Math.PI/2, 0, 0);
 
